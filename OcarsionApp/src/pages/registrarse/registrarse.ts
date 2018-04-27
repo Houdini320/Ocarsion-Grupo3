94cc1dce83cd6748ac0f7b251daa7579b69eee93
Paloma Valverde <100346229@alumnos.uc3m.es>
2018-04-27 18:05:01 +0200

    validacion registro

--- OcarsionApp/src/pages/registrarse/registrarse.ts
+++ OcarsionApp/src/pages/registrarse/registrarse.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 /**
  * Generated class for the RegistrarsePage page.
@@ -15,7 +16,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegistrarsePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  myForm: FormGroup;
+
+  constructor(public navCtrl: NavController,  public fb: FormBuilder) {
+    
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', [Validators.required, Validators.pattern(/^[a-z0-9]{6,18}$/)]],
+    }); 
   }
 
   ionViewDidLoad() {
