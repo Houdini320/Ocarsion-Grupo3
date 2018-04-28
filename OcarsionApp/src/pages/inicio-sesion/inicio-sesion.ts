bb1942af6266dd434395170233af19c2fc08a239
Elena Martinez <100346205@alumnos.uc3m.es>
2018-04-28 10:38:25 +0200

    inicio sesion
    he borrado uno de los metodos de ir a la pagina siguiente

--- OcarsionApp/src/pages/inicio-sesion/inicio-sesion.ts
+++ OcarsionApp/src/pages/inicio-sesion/inicio-sesion.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { MyApp } from '../../app/app.component';
 
 /**
  * Generated class for the InicioSesionPage page.
@@ -27,18 +28,25 @@ export class InicioSesionPage {
       }); 
     }
   
-   irPagSiguiente()
-    {
-    this.navCtrl.push('PrincipalUsuarioPage');
-    }
+    public irPagSiguiente(){
     
-    irRegistrarse()
-    {
+      //var cipEmpleado: string = ("#userEmail").valueOf;
+      //var cipEmpleado = new String(('#userEmail').valueOf);
+   
+      //var index = cipEmpleado.localeCompare('hola@hola.com');
+
+      //let cipEmpleado : string = (document.getElementById('userEmail')).nodeValue;
+
+      //let cipEmpleado : string = document.getElementById('userEmail').nodeValue;
+     //let cip : string = 
+
+ 
+        this.navCtrl.push('PrincipalUsuarioPage');
+    }
+    public irRegistrarse(){
       this.navCtrl.push('RegistrarsePage');
     }
-    
-
-   }
+}
    
  
 
