17da554da40fc1c6cb840f3c20acbc7cac372214
Elena Martinez <100346205@alumnos.uc3m.es>
2018-04-28 11:59:08 +0200

    registro completado

--- OcarsionApp/src/pages/registrarse/registrarse.ts
+++ OcarsionApp/src/pages/registrarse/registrarse.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, UrlSerializer } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the RegistrarsePage page.
@@ -18,7 +19,7 @@ export class RegistrarsePage {
 
   myForm: FormGroup;
 
-  constructor(public navCtrl: NavController,  public fb: FormBuilder) {
+  constructor(public navCtrl: NavController,  public fb: FormBuilder, public alertCtrl: AlertController) {
     
     this.myForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -26,11 +27,51 @@ export class RegistrarsePage {
     }); 
   }
 
-  ionViewDidLoad() {
+  public ionViewDidLoad() {
     console.log('ionViewDidLoad RegistrarsePage');
   }
-  irPagSiguiente(){
+
+  public irPagSiguiente(){
     this.navCtrl.push('PrincipalUsuarioPage');
-    }
+  }
+
+  public doPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Confirmación de registro',
+      message: "Si usted es un administrador introduzca su CIP de empleado",
+      inputs: [
+        {
+          name: 'CIP',
+          placeholder: 'CIP'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Registrarse como administrador',
+          handler: data => {
+            this.doAlert();
+            this.navCtrl.push('PrincipalAdministradorPage');
+          }
+        },
+        {
+          text: 'No soy administrador',
+          handler: data => {
+            this.doAlert();
+            this.navCtrl.push('PrincipalUsuarioPage');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  public doAlert() {
+    let alert = this.alertCtrl.create({
+      title: '¡Registro completado!',
+      message: '¡Gracias por unirte a esta gran familia del mundo del automóvil!',
+      buttons: ['Ok']
+    });
+    alert.present()
+  }
 
 }
